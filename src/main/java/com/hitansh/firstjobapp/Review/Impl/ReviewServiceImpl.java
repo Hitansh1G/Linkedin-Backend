d392611371c5235eb4e431e53b159e0d95f8ef46
Hitansh <hitansh.gupta@kristalball.com>
2024-05-30 19:44:17 +0530

    review class

---
+++ src/main/java/com/hitansh/firstjobapp/Review/Impl/ReviewServiceImpl.java
@@ -0,0 +1,6 @@
+package com.hitansh.firstjobapp.Review.Impl;
+
+import org.springframework.stereotype.Service;
+
+public class ReviewServiceImpl {
+}
---
+++ src/main/java/com/hitansh/firstjobapp/Review/ReviewService.java
@@ -0,0 +1,7 @@
+package com.hitansh.firstjobapp.Review;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ReviewService {
+}
