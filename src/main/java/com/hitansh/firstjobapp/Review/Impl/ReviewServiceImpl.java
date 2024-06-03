90574fadde69a12d236f7cf0c30ba4ddc27f21ed
Hitansh <hitansh.gupta@kristalball.com>
2024-06-03 11:26:41 +0530

    getting all reviews of a company

--- src/main/java/com/hitansh/firstjobapp/Review/Impl/ReviewServiceImpl.java
+++ src/main/java/com/hitansh/firstjobapp/Review/Impl/ReviewServiceImpl.java
@@ -1,6 +1,22 @@
 package com.hitansh.firstjobapp.Review.Impl;
 
+import com.hitansh.firstjobapp.Review.Review;
+import com.hitansh.firstjobapp.Review.ReviewRepository;
+import com.hitansh.firstjobapp.Review.ReviewService;
 import org.springframework.stereotype.Service;
 
-public class ReviewServiceImpl {
+import java.util.List;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public  List<Review> getAllReviews(Long companyId) {
+        return reviewRepository.findByCompanyId(companyId);
+    }
 }
